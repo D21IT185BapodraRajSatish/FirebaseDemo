28bdae258a04fe031132472763bb84a40ee58eeb
RAJ BAPODRA <d21it185@charusat.edu.in>
2023-03-07 12:01:23 +0530

    Sign in With Google on going

---
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.firebasedemo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button submitBtn;
+    SignInButton signInButton;
+    BeginSignInRequest signInRequest;
+
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.main_activity_email);
+        password = findViewById(R.id.main_activity_password);
+        submitBtn = findViewById(R.id.main_activity_submitButton);
+        signInButton= findViewById(R.id.sign_in_button);
+
+        mAuth = FirebaseAuth.getInstance();
+        signInButton.setOnClickListener(v->{
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
+
+
+        });
+
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+
+}
